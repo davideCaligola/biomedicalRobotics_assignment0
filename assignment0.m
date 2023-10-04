80a011780b496a570299253960fd9e2e8d798707
davideCaligola <dvd.c@libero.it>
2023-10-04 10:56:26 +0200

    Added some comments on signals

--- assignment0.m
+++ assignment0.m
@@ -35,6 +35,17 @@ subtitle("samples from 30000 to 32000");
 xlabel("frequency [HZ]");
 grid on;
 
+%% comments on data 1
+% - base noisy
+% - zero mean
+% - activation observable due to different amplitude
+% - difficulty to recognize a clear threshold for determine when an
+%   activation starts
+% - different levels of activations
+% - looking at the time series, it is difficult to estimate the content
+%   frequency
+% - missing unit on amplitude, so it is not possible evaluate it
+
 %% plot data2
 figure();
 plot(t2, data2(1,:)); hold on;
@@ -43,9 +54,23 @@ grid on;
 title("Data 2 - Motion");
 xlabel("time [s]");
 
+%% comments on data 2
+% - signal clean, apparently without noise
+% - dynamics slower than data 1 and data 2
+% - presence of constant levels
+% - presence of peaks and valleys
+% - missing unit on amplitude, so it is not possible evaluate it
+
 %% plot data3
 figure();
 plot(t3, data3);
 grid on;
 title("Data 3 - EEG");
 xlabel("time [s]");
+
+%% comments on data 3
+% - noisy
+% - not a clear pattern
+% - lower frequency than data 1
+% - not a clear mean
+% - missing unit on amplitude, so it is not possible evaluate it
